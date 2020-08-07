0f9f44145c846de1a1682b2b4076dd928eda25f5
Santiago Bermudez <santiagobermudezparra@gmail.com>
2020-08-07 10:53:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GifExpertApp
+Npm Start && install
+Una pequeña aplicación hecha en React con Hooks para buscar y mostrar gifs animados.
---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
+
+
+describe('Pruebas en <GifGridItem />', () => {
+
+    
+    
+    test('debe de mostrar el componente correctamente', () => {
+        
+
+        const wrapper = shallow(<GifGridItem title='title'/>);
+        expect(wrapper).toMatchSnapshot();
+
+    })
+
+    
+    
+    
+    
+
+})
